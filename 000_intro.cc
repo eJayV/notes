32bfa8b9ece8f73e53838a7ea978de24cc037c40
Esteban Vasco <estebanjvasco@csu.fullerton.edu>
2023-02-16 09:43:37 -0800

    wip

---
+++ 000_intro.cc
@@ -0,0 +1,23 @@
+/* Feb 16, 2023
+
+Computer Setup
+  For the CSUF CPSC120 course it is recommended use
+  Ubuntu 20.04 (can be found here: https://bit.ly/3x4Mrpn )
+  and run the following command in terminal to download the
+  needed/recommended tools/programs/files/etc for the course:
+  wget -q https://raw.githubusercontent.com/mshafae/tusk/main/quickinstall.sh -O- | sh
+
+  For CPSC121, there is the additional option of
+  https://replit.com/ which is an in browser alternative to
+  using Visual Studio Code (VSCode).
+
+
+  
+*/
+
+
+
+
+int main() {
+  return 0;
+}
